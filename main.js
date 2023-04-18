efe0b74a9fcb4f358aebe7c7b5f520fc11431caa
Sinbadd87 <dtishen87@gmail.com>
2023-04-18 22:42:19 +0300

    Create navbar

--- main.js
+++ main.js
@@ -29,3 +29,35 @@ contacts.addEventListener('click', function(){
   skills.style.color = '#2708A0';
   contacts.style.color = '#4316F3';
 });
+
+//Slide underline
+const tabs = document.querySelectorAll('.item');
+const highlight = document.querySelector('#marker');
+const firstTab = tabs[0];
+
+// Calculate the position of the highlight element
+const left = firstTab.getBoundingClientRect().left - 28.5;
+const width = firstTab.offsetWidth + 8;
+
+// Set the default position of the highlight element
+highlight.style.left = `${left}px`;
+highlight.style.width = `${width}px`;
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', function() {
+    // Get the index of the selected tab
+    const index = this.dataset.index;
+    
+    // Calculate the position of the highlight element
+    const left = this.offsetLeft;
+    const width = this.offsetWidth;
+    
+    // Update the position of the highlight element
+    highlight.style.left = `${left}px`;
+    highlight.style.width = `${width}px`;
+    
+    // Add active class to the selected tab
+    tabs.forEach(tab => tab.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
